31d4743257f56cb72ee7977b87e9b8ebfe1b68f2
Tom Wayson <tom@tomwayson.com>
2020-02-02 22:53:03 -0800

    convert App from class-based component to function w/ useState()

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import EsriMap from "./EsriMap";
 
-class App extends React.Component {
-  state = {
-    theme: "light",
-    mapLoaded: false
+function App () {
+  const [theme, setTheme] = useState('light');
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  const switchTheme = e => {
+    setTheme(e.target.dataset.theme);
   };
-  switchTheme = e => {
-    this.setState({ theme: e.target.dataset.theme });
+  const onMapLoad = () => {
+    setMapLoaded(true);
   };
-  onMapLoad = () => {
-    this.setState({ mapLoaded: true });
-  };
-  render() {
-    const appClassName = `App ${this.state.theme === "dark" ? "dark" : ""}`;
-    return (
-      <div className={appClassName}>
-        <div>
-          <label>Theme: </label>
-          <button
-            onClick={this.switchTheme}
-            data-theme="light"
-            disabled={!this.state.mapLoaded}
-          >
-            Light
-          </button>
-          <button
-            onClick={this.switchTheme}
-            data-theme="dark"
-            disabled={!this.state.mapLoaded}
-          >
-            Dark
-          </button>
-        </div>
-        <EsriMap theme={this.state.theme} onLoad={this.onMapLoad} />
+  const appClassName = `App ${theme === "dark" ? "dark" : ""}`;
+  return (
+    <div className={appClassName}>
+      <div>
+        <label>Theme: </label>
+        <button
+          onClick={switchTheme}
+          data-theme="light"
+          disabled={!mapLoaded}
+        >
+          Light
+        </button>
+        <button
+          onClick={switchTheme}
+          data-theme="dark"
+          disabled={!mapLoaded}
+        >
+          Dark
+        </button>
       </div>
-    );
-  }
+      <EsriMap theme={theme} onLoad={onMapLoad} />
+    </div>
+  );
 }
 
 export default App;
