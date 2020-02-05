8b39add793ae1e6836126bf0a6ca3347650db290
Tom Wayson <tom@tomwayson.com>
2020-02-05 06:57:29 -0800

    use separate effect for onLoad

--- src/EsriMap.js
+++ src/EsriMap.js
@@ -12,19 +12,17 @@ function EsriMap ({ theme, onLoad }) {
   // we'll hold a reference to the map view in state
   const [view, setView] = useState(null);
   // need initial prop values when loading the map (see below)
-  const initialProps = useRef({ theme, onLoad });
+  const initialProps = useRef({ theme });
 
   // load the ArcGIS JS API and map when the component mounts
   useEffect(() => {
     // we need a closure scope so we can access the view in destroyView()
     let _view;
     // hooks require inline functions in order to use async/await
-    async function initMap ({ theme, onLoad }) {
+    async function initMap ({ theme }) {
       // we need a reference to the view in other hooks
       _view = await loadMap(mapDiv.current, themeToBasemap(theme));
       setView(_view);
-      // let the app know the map has loaded
-      onLoad && onLoad();
     }
     // we can't use the props directly in this hook w/o depending on them
     // and we can't depend on them if we want this hook to only run once
@@ -46,6 +44,13 @@ function EsriMap ({ theme, onLoad }) {
       view.map.basemap = themeToBasemap(theme);
     }
   }, [view, theme]);
+
+  // let the app know the map has loaded
+  useEffect(() => {
+    if (view && onLoad) {
+      onLoad();
+    }
+  }, [view, onLoad]);
 
   // render a div to use as the view's container
   return <div className="esri-map" ref={mapDiv} />;
